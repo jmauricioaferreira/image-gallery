4604d00da4f317a2198e05f873a86639fae645a0
Jose Mauricio <jmauricio.a.ferreira@gmail.com>
2020-12-29 21:54:20 -0300

    creating ImageCard

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import ImageCard from './Components/ImageCard';
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo`,
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      Welcome
-      <img src="https://source.unsplash.com/random" alt="" />
-      <div className="px-6 py-4">
-        So are tou
-        <div className="font-bold text-purple-500 text-xl mb-2">
-          Wlecome to my wordl
-        </div>
-        <ul>
-          <li>
-            <strong>Views: </strong>
-            4000
-          </li>
-          <li>
-            <strong>Dowloads: </strong>
-            4000
-          </li>
-          <li>
-            <strong>Likes: </strong>
-            4000
-          </li>
-        </ul>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+        {images.map((image) => (
+          <ImageCard key={image.id} image={image} />
+        ))}
       </div>
     </div>
   );
---
+++ src/Components/ImageCard.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const ImageCard = ({ image, user }) => {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      Welcome
+      <img src={image.webformatURL} alt="" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Photo by {image.user}
+        </div>
+        <ul>
+          <li>
+            <strong>Views: </strong>
+            {image.views}
+          </li>
+          <li>
+            <strong>Dowloads: </strong>
+            {image.downloads}
+          </li>
+          <li>
+            <strong>Likes: </strong>
+            {image.likes}
+          </li>
+        </ul>
+      </div>
+      <div className="px-6 px-4">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag1
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag2
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag3
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default ImageCard;
