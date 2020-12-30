b3cd783c66a1dc7717b3e97c5d9e0be0bf487d23
Jose Mauricio <jmauricio.a.ferreira@gmail.com>
2020-12-29 21:09:43 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+function App() {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      Welcome
+      <img src="https://source.unsplash.com/random" alt="" />
+      <div className="px-6 py-4">
+        So are tou
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Wlecome to my wordl
+        </div>
+        <ul>
+          <li>
+            <strong>Views: </strong>
+            4000
+          </li>
+          <li>
+            <strong>Dowloads: </strong>
+            4000
+          </li>
+          <li>
+            <strong>Likes: </strong>
+            4000
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
